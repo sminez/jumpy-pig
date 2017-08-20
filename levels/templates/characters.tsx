dd080bd5590d0cd2597a38f197330b65925f8d38
sminez <innes@cocoon.life>
2017-08-20 22:30:32 +0100

    adding new animations and starting to use Tiled

---
+++ levels/templates/characters.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="characters" tilewidth="60" tileheight="80" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="60" height="41" source="../../assets/sprites/alpha-pig-left-light.png"/>
+ </tile>
+ <tile id="1">
+  <image width="60" height="41" source="../../assets/sprites/alpha-pig-left-up-light.png"/>
+ </tile>
+ <tile id="2">
+  <image width="60" height="41" source="../../assets/sprites/alpha-pig-right-up-light.png"/>
+ </tile>
+ <tile id="3">
+  <image width="60" height="41" source="../../assets/sprites/alpha-pig-right-light.png"/>
+ </tile>
+ <tile id="4">
+  <image width="59" height="80" source="../../assets/sprites/donkey.png"/>
+ </tile>
+</tileset>
